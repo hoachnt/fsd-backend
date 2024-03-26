29228b49cb8c60e1a33b06da5cabceae34b5e773
hoach-linux <hoachprogrammer@gmail.com>
2024-03-26 14:45:49 +0700

    added patch and delete api

--- back_django/apps/tasks/urls.py
+++ back_django/apps/tasks/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'tasks'
 urlpatterns = [
     path('api/', views.api, name = 'api'),
+    path('api/<int:task_id>/', views.task_by_id, name = 'task_by_id'),
 ]
--- back_django/apps/tasks/views.py
+++ back_django/apps/tasks/views.py
@@ -36,6 +36,38 @@ def post_request(body):
         return JsonResponse({'error': f'Missing required field: {e}'}, status=400)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
+    
+def patch_request(body, task_id):
+    task = Task.objects.get(id = task_id)
+    try:
+
+        # Проверяем наличие необязательного поля 'checked'
+        checked = body.get('checked', task.checked)
+        title = body.get('title', task.title)
+        description = body.get('description', task.description)
+        date_start = body.get('date_start', task.date_start)
+        date_end = body.get('date_end', task.date_end)
+        date_time = body.get('date_time', task.date_time)
+
+        # Создаем новый объект Task
+        
+        task.title=title
+        task.description=description
+        task.checked=checked
+        task.date_start=date_start
+        task.date_end=date_end
+        task.date_time=date_time
+        
+        task.save()
+        
+        task_json = list(Task.objects.filter(id=task.id).values())[0]
+    
+        return JsonResponse({'data': task_json})
+    except KeyError as e:
+        return JsonResponse({'error': f'Missing required field: {e}'}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
 # Create your views here.
 @csrf_exempt
 def api(request):
@@ -46,4 +78,19 @@ def api(request):
         body = json.loads(body_unicode)
         
         return post_request(body)
+    else:
+        return JsonResponse({'error': "Request not supported"}, status=405)
+    
+@csrf_exempt
+def task_by_id(request, task_id):
+    if request.method == "PATCH":
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
         
+        return patch_request(body, task_id)
+    elif request.method == "DELETE":
+        task = Task.objects.get(id=task_id).delete()
+        
+        return HttpResponse(task_id)
+    else:
+        return JsonResponse({'error': "Request not supported"}, status=405)
